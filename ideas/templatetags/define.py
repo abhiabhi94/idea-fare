fab51b1cd68de2116401ca1ee115de95c5e8ca62
abhiabhi94 <abhy.abhy94@gmail.com>
2020-03-11 05:52:18 +0530

    Docstring is more readable now

--- ideas/templatetags/define.py
+++ ideas/templatetags/define.py
@@ -7,8 +7,11 @@ register = template.Library()
 def define(val=None):
     """
     Define a "define" tag to be used for assigning values inside a template
-    Usage:  {{% load define %}}
 
-    {% define "something" as var %} -> This will store the value "something" within the variable var inside the template.
+    Usage:  
+        {% load define %}
+
+        {% define "something" as var %} -> This will store the value "something" within the\
+            variable var inside the template.
     """
     return val
