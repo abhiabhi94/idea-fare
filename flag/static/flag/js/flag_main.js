8b736afb806e3ab531d0fffad6422be32089fdcf
abhiabhi94 <abhy.abhy94@gmail.com>
2020-04-25 00:24:53 +0530

    Make the flag template pluggable to any other model
    - elements are grabbed in javascript according to their class name rather than id
    - remove loading of static assets from flag template
    - static assets are now loaded inside the template that needs to use the flag model -> assets are now loaded only once per page

--- flag/static/flag/js/flag_main.js
+++ flag/static/flag/js/flag_main.js
@@ -1,34 +1,53 @@
 'use strict';
 
-const modal = document.getElementById("report-modal");
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+let parents = document.getElementsByClassName("report-modal-form-combined");
+for (let parent of parents) {
+    let modal = parent.querySelector('.flag-report-modal');
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            console.log(modal);
+            modal.style.display = "none";
+        }
+    };
 
-const btn = document.getElementById("report-button");
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+    let btns = document.getElementsByClassName("flag-report-button");
+    for (let btn of btns) {
+        btn.onclick = function() {
+            modal.style.display = "block";
+        };
+    };
 
-const span = document.getElementById("report-modal-close");
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+    let spans = document.getElementsByClassName("report-modal-close");
+    for (let span of spans) {
+        span.onclick = function() {
+            modal.style.display = "none";
+        };
+    };
 
-const lastFlagReason = document.getElementById('last-flag-reason')
+    // when the user clicks on the last reason , open the comment box
+    let flagForms = document.getElementsByClassName('report-modal-form');
+    for (let flagForm of flagForms) {
+        let lastFlagReason = flagForm.querySelector('.last-flag-reason');
+        let flagComment = flagForm.querySelector('.report-modal-form-comment');
+        flagForm.onchange = function(event) {
+            if (event.target.value === lastFlagReason.innerHTML) {
+                flagComment.required = true;
+                flagComment.style.display = "block";
+            } else {
+                flagComment.style.display = "none";
+            }
+        };
+    };
+};
 
-// when the user clicks on the last reason , open the comment box
-const flagForm = document.getElementById('report-modal-form');
-const flagComment = document.getElementById('report-modal-form-comment');
-flagForm.onchange = function(event) {
-    if (event.target.value === lastFlagReason.innerHTML) {
-        flagComment.required = true;
-        flagComment.style.display = "block";
-    } else {
-        flagComment.style.display = "none";
-    }
-}
+
+
+// flagForm.onchange = function(event) {
+//     if (event.target.value === lastFlagReason.innerHTML) {
+//         flagComment.required = true;
+//         flagComment.style.display = "block";
+//     } else {
+//         flagComment.style.display = "none";
+//     }
+// }
